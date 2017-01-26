d252d4df7fc98084e459ec97ba7763ab7d320cbe
b1anker <narutowontstop@gmail.com>
2017-01-26 23:29:45 +0800

    Beautify the code

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const Koa = require('koa');
-const app = new Koa();
+const koa = require('koa');
+const app = koa();
+
+// x-response-time
+app.use(function*(next) {
+  console.log('line 1');
+  const start = new Date;
+  yield next;
+  console.log('line 5');
+  const ms = new Date - start;
+  this.set('X-Response-Time', `${ms}ms`);
+});
+
+// log time
+app.use(function*(next) {
+  console.log('line 2');
+  const start = new Date;
+  yield next;
+  console.log('line4');
+  const ms = new Date - start;
+  console.info('%s %s : %s ms', this.method, this.url, ms);
+});
 
 // response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+app.use(function*(next) {
+  console.log('line3');
+  this.body = 'hello world';
 });
 
 app.listen(3000);
